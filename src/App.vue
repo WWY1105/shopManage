30cb9fc8b6434c1bce6a7dbd3967c7b1df7e3dcb
WWY1105 <wwy19931105@163.com>
2020-11-16 08:43:43 +0800

    1116

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-aside width="260px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+
+</style>
